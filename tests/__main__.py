d6f5b70171e826ceade8e88ad7a670d1d8f14589
Javier Trivino <Javier.Trivino@trivago.com>
2018-03-21 17:58:29 +0100

    Added new test

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,5 @@
 import unittest
-from pipeline_simulator.core import memories, architectures, instructions
+from pipeline_simulator.core import memories, architectures, instructions, compilers
 
 
 class TestPipelineMethods(unittest.TestCase):
@@ -12,7 +12,7 @@ class TestPipelineMethods(unittest.TestCase):
         source_file = 'tests/programs/code_test_parser.txt'
         registers = memories.RegisterSet()
         memory = memories.Memory(1024)
-        parser = instructions.Parser(registers=registers, memory=memory)
+        parser = compilers.Parser(registers=registers, memory=memory)
         program = parser.parse(source_file)
 
         " Correct instruction class "
@@ -45,27 +45,12 @@ class TestPipelineMethods(unittest.TestCase):
         self.assertEqual(program[3]._rd, registers.get(8))
         self.assertEqual(program[3]._offset, 599)
 
-        self.assertEqual(program[4]._rs, registers.get(1))
-        self.assertEqual(program[4]._rd, registers.get(22))
-        self.assertEqual(program[4]._offset, 993)
-
-        # Branch
-        self.assertEqual(program[5]._rs, registers.get(1))
-        self.assertEqual(program[5]._rt, registers.get(2))
-        self.assertEqual(program[5]._imm, 7)
-
-        self.assertEqual(program[6]._rs, registers.get(9))
-        self.assertEqual(program[6]._rt, registers.get(5))
-        self.assertEqual(program[6]._imm, 3)
-
-        # Jump
-        self.assertEqual(program[7]._imm, 7)
-
+    """ Short program with dependencies """
     def test_pipeline_code1(self):
         source_file = 'tests/programs/code1.txt'
         registers = memories.RegisterSet(registers_file='tests/programs/registers1.txt')
         memory = memories.Memory(1024)
-        parser = instructions.Parser(registers=registers, memory=memory)
+        parser = compilers.Parser(registers=registers, memory=memory)
         program = parser.parse(source_file)
         memory.write_program(program)
         cpu_instance = architectures.PipelinedCpu(registers=registers, memory=memory)
@@ -101,14 +86,10 @@ class TestPipelineMethods(unittest.TestCase):
         source_file = 'tests/programs/code2.txt'
         registers = memories.RegisterSet(registers_file='tests/programs/registers2.txt')
         memory = memories.Memory(2048)
-        parser = instructions.Parser(registers=registers, memory=memory)
+        parser = compilers.Parser(registers=registers, memory=memory)
         program = parser.parse(source_file)
         memory.write_program(program)
         cpu_instance = architectures.PipelinedCpu(registers=registers, memory=memory)
-
-        registers.get(0).set(0)
-        registers.get(1).set(1)
-        registers.get(11).set(11)
 
         cpu_instance.start()
         while not cpu_instance.is_halted():
@@ -123,6 +104,25 @@ class TestPipelineMethods(unittest.TestCase):
         for i in range(10):
             for j in range(10):
                 self.assertEqual(memory.get_data(1000+i*10+j), (i+1)*(j+1))
+
+    """ Short program without dependencies """
+    def test_pipeline_code3(self):
+        source_file = 'tests/programs/code3.txt'
+        registers = memories.RegisterSet(registers_file='tests/programs/registers3.txt')
+        memory = memories.Memory(2048)
+        parser = compilers.Parser(registers=registers, memory=memory)
+        program = parser.parse(source_file)
+        memory.write_program(program)
+        cpu_instance = architectures.PipelinedCpu(registers=registers, memory=memory)
+
+        cpu_instance.start()
+        while not cpu_instance.is_halted():
+            cpu_instance.step()
+
+        self.assertEqual(registers.get(1).get_data(), 5)
+        self.assertEqual(registers.get(2).get_data(), -1)
+        self.assertEqual(registers.get(3).get_data(), 3)
+        self.assertEqual(memory.get_data(1003), 3)
 
 
 if __name__ == '__main__':
